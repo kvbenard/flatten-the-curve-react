ccf7d8e52c43b4d635e3a300b4900e03a723e6ad
Kevin Benard <kv.benard@gmail.com>
2020-05-02 15:00:11 +0200

    Updates

---
+++ precache-manifest.4912334b467131c88921fb0223b90e5d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0373d03691655f0a33fc93dcbafb1f88",
+    "url": "/flatten-the-curve-react/index.html"
+  },
+  {
+    "revision": "af75affc1cbb619b7e4d",
+    "url": "/flatten-the-curve-react/static/css/main.23c19fb0.chunk.css"
+  },
+  {
+    "revision": "f1a3d70998bbe4555749",
+    "url": "/flatten-the-curve-react/static/js/3.37f186ca.chunk.js"
+  },
+  {
+    "revision": "d3154195c32512e604b8",
+    "url": "/flatten-the-curve-react/static/js/4.ff951540.chunk.js"
+  },
+  {
+    "revision": "8c91cf6b76627cf95627c15722a379c1",
+    "url": "/flatten-the-curve-react/static/js/4.ff951540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3509adebebca59374699",
+    "url": "/flatten-the-curve-react/static/js/5.14d57dac.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/flatten-the-curve-react/static/js/5.14d57dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caad19abded4f6940293",
+    "url": "/flatten-the-curve-react/static/js/6.ee624112.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/flatten-the-curve-react/static/js/6.ee624112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af75affc1cbb619b7e4d",
+    "url": "/flatten-the-curve-react/static/js/main.5700ddcd.chunk.js"
+  },
+  {
+    "revision": "d09edd85a210c75a2f1b",
+    "url": "/flatten-the-curve-react/static/js/runtime-main.c87d2380.js"
+  },
+  {
+    "revision": "e6b507d39e1fe1fa3880",
+    "url": "/flatten-the-curve-react/static/js/xlsx.3d699e01.chunk.js"
+  }
+]);
